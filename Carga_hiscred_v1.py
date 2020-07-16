672115e3f149f6c9e04598bcd5b3cda6103eb24f
Francisco Espinosa - Lap <50310159+PacoEspinosa@users.noreply.github.com>
2020-07-15 21:17:15 -0500

    minor update
    processing hiscred files

--- Carga_hiscred_v1.py
+++ Carga_hiscred_v1.py
@@ -23,7 +23,7 @@ warnings.simplefilter('ignore')
 
 #Constantes
 date1 = '2020-01-01'  
-date2 = '2020-06-30'
+date2 = '2020-07-30'
 transact_table = 'Transacciones_TDC_2020'
 operative_table = 'Operativas'
 filepattern = 'his_credito'
