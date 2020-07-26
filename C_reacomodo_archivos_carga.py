5a867028e9a4e54868253b3bc6e47d7f27a49506
Francisco Espinosa - Lap <50310159+PacoEspinosa@users.noreply.github.com>
2020-07-26 11:40:49 -0500

    run for every year
    updated due to running

--- C_reacomodo_archivos_carga.py
+++ C_reacomodo_archivos_carga.py
@@ -9,16 +9,16 @@ import pandas as pd
 import warnings
 
 #path = os.getcwd() #obtiene el directorio de trabajo actual
-path = 'H:\\hiscred\\test'
+path = 'C:\\Users\\fespinosa\\Documents\\Trabajo\\DWH\\Datos\\hiscred_oro'
 files = []
 
 warnings.simplefilter('ignore')
 
 date1 = '2019-01-01'  
 date2 = '2019-12-31'
-filepattern = 'his_credito'
+filepattern = 'Hiscred_Oro'
 datepattern = "%m%d%Y"
-fileext = ".gz"
+fileext = ".txt"
 
 for r, d, f in os.walk(path):
     for file in f:
@@ -29,7 +29,7 @@ datelist = pd.date_range(date1,date2).tolist()
 #crea subdirectorio destino
 os.mkdir(path + '\\' + date1[0:4])
 
-files_list = open(r"MyFile2.txt","w+") 
+files_list = open(r"MyFile" + date1[0:4] + ".txt","w+") 
 for date in datelist:
     filename = filepattern + date.strftime(datepattern) + fileext
     if filename in files:
