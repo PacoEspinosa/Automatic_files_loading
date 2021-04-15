6e89368e02b017b17ac81e093c7f92e5e1e46483
Francisco Espinosa - Lap <50310159+PacoEspinosa@users.noreply.github.com>
2021-04-14 20:59:04 -0500

    Procesos carga

---
+++ Carga_mensual.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  5 11:51:10 2021
+
+@author: fespinosa
+"""
+
+
+exec(open("Carga_dg_email_v1.py").read())
+exec(open("Carga_dg_nombres_v1.py").read())
+exec(open("Carga_dg_telefonos_v1.py").read())
+exec(open("Carga_dg_nombres_capta_v1.py").read())
+exec(open("Carga_dg_celular_capta.py").read())
+exec(open("Carga_dg_email_capta.py").read())
+exec(open("Carga_dg_plasticos.py").read())
+exec(open("Carga_portafolio_capta.py").read())
